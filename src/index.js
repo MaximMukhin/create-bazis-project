06fa2b4d6a7c0c4f2f5ac06df6055760e3433542
MaximMukhin <eriwer@gmail.com>
2022-07-28 11:49:40 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // import express from 'express'
 // import articles from "./articles.js";
 
-
 // const app = express();
 // const PORT = 3000;
 //
@@ -16,19 +15,47 @@
 
 // база артикулов
 const articles2 = [
-    { article: 'ADELINA-P-T02-60-2B-MR960', series: 'ADELINA-P-T02', link: '\\Серия\\AcquaCraft\\Bella\\Bella_SF027\\Bella-F21L-106-SF027.b3d' },
-    { article: 'BELLA-L-F21-106-SF027-L', series: 'BELLA-L-F21', link: '\\Серия\\AcquaCraft\\Bella\\Bella_SF027\\Bella-F21L-106-SF027.b3d' },
-    { article: 'AURORA-800-2C-SO-RT', series: 'Aurora', link: '\\Серия\\Мебель для ванной\\AU_KR\\(RT) Rovere Tabacco - Дуб Галифакс табак H1181 ST37\\AU45-80_RT.b3d' },
-    { article: 'CADRO-80-1C-SO-BG-BLUM', series: 'Cadro', link: '\\Серия\\Мебель для ванной\\Cadro\\(BG) Bianco Ghiaccio - Белый лёд Atlas OSS\\Cadro-80_BG.b3d' },
-    { article: 'KRAFT 39-500/390-2C-SO-BO', series: 'Kraft 39', link: '\\Серия\\Мебель для ванной\\AU_KR\\(BO) Bianco Opaco - Белый базовый W908 SM\\KR39-50_BO.b3d' },
-    { article: 'KRAFT 39-500/390-2C-SO-CG', series: 'Kraft 39', link: '\\Серия\\Мебель для ванной\\AU_KR\\(CG) Cemento Grigio - Бетон D3274 SM\\KR39-50_CG.b3d' },
-    { article: 'KRAFT 39-500/390-2C-SO-RNN', series: 'Kraft 39', link: '\\Серия\\Мебель для ванной\\AU_KR\\(RNN) Rovere Nebrasca Nature - Дуб Небраска натуральный H3331 ST10\\KR39-50_RNN.b3d' },
+  {
+    article: "ADELINA-P-T02-60-2B-MR960",
+    series: "ADELINA-P-T02",
+    link: "\\Серия\\AcquaCraft\\Bella\\Bella_SF027\\Bella-F21L-106-SF027.b3d",
+  },
+  {
+    article: "BELLA-L-F21-106-SF027-L",
+    series: "BELLA-L-F21",
+    link: "\\Серия\\AcquaCraft\\Bella\\Bella_SF027\\Bella-F21L-106-SF027.b3d",
+  },
+  {
+    article: "AURORA-800-2C-SO-RT",
+    series: "Aurora",
+    link: "\\Серия\\Мебель для ванной\\AU_KR\\(RT) Rovere Tabacco - Дуб Галифакс табак H1181 ST37\\AU45-80_RT.b3d",
+  },
+  {
+    article: "CADRO-80-1C-SO-BG-BLUM",
+    series: "Cadro",
+    link: "\\Серия\\Мебель для ванной\\Cadro\\(BG) Bianco Ghiaccio - Белый лёд Atlas OSS\\Cadro-80_BG.b3d",
+  },
+  {
+    article: "KRAFT 39-500/390-2C-SO-BO",
+    series: "Kraft 39",
+    link: "\\Серия\\Мебель для ванной\\AU_KR\\(BO) Bianco Opaco - Белый базовый W908 SM\\KR39-50_BO.b3d",
+  },
+  {
+    article: "KRAFT 39-500/390-2C-SO-CG",
+    series: "Kraft 39",
+    link: "\\Серия\\Мебель для ванной\\AU_KR\\(CG) Cemento Grigio - Бетон D3274 SM\\KR39-50_CG.b3d",
+  },
+  {
+    article: "KRAFT 39-500/390-2C-SO-RNN",
+    series: "Kraft 39",
+    link: "\\Серия\\Мебель для ванной\\AU_KR\\(RNN) Rovere Nebrasca Nature - Дуб Небраска натуральный H3331 ST10\\KR39-50_RNN.b3d",
+  },
 ];
 
 // console.log(articles2) //=
 // console.log(articles2[0].link)
 // заказ клиента
-//const coll = [
+//const articles = [
 //'AURORA-800-2C-SO-RT	7',
 //'CADRO-80-1C-SO-BG-BLUM	3',
 //'KRAFT 39-500/390-2C-SO-BO	5',
@@ -36,84 +63,80 @@ const articles2 = [
 //'KRAFT 39-500/390-2C-SO-RNN	13',
 //];
 
+const articles = []; // заказ клиента
+const articlesObj = []; // заказ клиента
 
-const coll = []  // заказ клиента 
-const collObj = [] // заказ клиента 
-
-console.log('coll-Start', coll) //=
+console.log("articles-Start", articles); //=
 
 // функция загрузки файла
 function readFile(input) {
-    let file = input.files[0];
-    
-    let reader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = function () {
-        //читаю файл
-        const res = reader.result
-        //разбиваю на массив
-        const strSlice = res.split('\n')
-        //console.log('strSlice', strSlice)
+  let file = input.files[0];
 
-        //добавляю в колекцию массив
-        for (let i = 0; i < strSlice.length; i++) {
-          coll.push(strSlice[i])
-        }
-        
-        console.log('coll.length', coll.length)
-        //разбиваю на обьект
-        collPars(coll)
-    };
-};
+  let reader = new FileReader();
+  reader.readAsText(file);
+  reader.onload = function () {
+    //читаю файл
+    const res = reader.result;
+    //разбиваю на массив
+    const strSlice = res.split("\n");
+    //console.log('strSlice', strSlice)
 
-console.log('coll-Center', coll)
+    //добавляю в колекцию массив
+    for (let i = 0; i < strSlice.length; i++) {
+      articles.push(strSlice[i]);
+    }
+    document.getElementById("text-file").innerHTML = res;
+
+    console.log("articles.length", articles.length);
+    //разбиваю на обьект
+    articlesPars(articles);
+    outList();
+    outList2();
+  };
+}
+
+console.log("articles-Center", articles);
 
 // разбиваем заказ клиента на обьекты
-const collPars = (arr) => {
-    //const result = []
-    const arrPars = []
-    console.log('arr', arr.length)
+const articlesPars = (arr) => {
+  //const result = []
+  const arrPars = [];
+  console.log("arr", arr.length);
 
-    //разбиваю строки на масивы артикул + количество
-    for (let i = 0; i < arr.length; i++) {
-        arrPars.push(coll[i].split('\t'))
-    }
+  //разбиваю строки на масивы артикул + количество
+  for (let i = 0; i < arr.length; i++) {
+    arrPars.push(articles[i].split("\t"));
+  }
 
-    for (let j = 0; j < arrPars.length; j++) {
+  for (let j = 0; j < arrPars.length; j++) {
     //    result.push({ article: arrPars[j][0], qty: arrPars[j][1] })
-        collObj.push({ article: arrPars[j][0], qty: arrPars[j][1] })
-    }
-    //return result //=
-    console.log('collObj', collObj)
+    articlesObj.push({ article: arrPars[j][0], qty: arrPars[j][1] });
+  }
+  //return result //=
+  console.log("articlesObj", articlesObj);
 };
-console.log('coll-End', coll)  //=
+console.log("articles-End", articles); //=
 
+const file = () => {
+  let result = "";
 
-const file = (objArt) => {
-
-    let result = '';
-
-    for (let i = 0; i < collObj.length; i += 1) {
-
-            const text = `
+  for (let i = 0; i < articlesObj.length; i += 1) {
+    const text = `
     <File>
       <Type>0</Type>
-      <Name>${articles2[i].link}</Name>
+      <Name>${"articles2[i].link"}</Name>
       <SubName/>
-      <Sign>${collObj[i].article}</Sign>
+      <Sign>${articlesObj[i].article}</Sign>
       <Note/>
       <Comment/>
-      <Count>${collObj[i].qty}</Count>
+      <Count>${articlesObj[i].qty}</Count>
     </File>
     `;
-            if (result === '') result = text;
-            else result += text;
-        }
-        console.log('file')
-    return result;
+    if (result === "") result = text;
+    else result += text;
+  }
+  return result;
 };
-
-file() //=
 
 const xml = `
 <?xml version="1.0" encoding="UTF-8"?>
@@ -129,10 +152,31 @@ const xml = `
     <EstimateInfo/>
   </DataProject>
 </Document>
-${console.log('xml')}
 `;
 
+console.log(xml);
+
+//отрисовкап списка
+const outList = () => {
+  let out = "";
+  for (article in articles) {
+    out += `<div>${articles[article]}</div>`;
+  }
+  document.getElementById("text-file-array").innerHTML = out;
+};
+
+const outList2 = () => {
+  let out2 = "";
+  for (article of articlesObj) {
+    out2 += `<div>art: ${article.article} - qty: ${article.qty}<div>`;
+  }
+  document.getElementById("text-file-object").innerHTML = out2;
+};
+
+const xmlNew = `<p>${file()}</p>`;
+document.getElementById("xml-file").innerHTML = xmlNew;
+console.log(xmlNew);
 
 // функция Скачки
-let blob = new Blob([xml], { type: 'text/plain' });
+let blob = new Blob([xml], { type: "text/plain" });
 link.href = URL.createObjectURL(blob);
---
+++ src/test.js
@@ -0,0 +1,74 @@
+//const obj = [
+//  {art: '111', qty: '1'},
+//  {art: '222', qty: '2'},
+//  {art: '333', qty: '3'},
+//  {art: '444', qty: '4'},
+//];
+//
+//let str = ''
+//for (const item of obj) {
+//  str += `<div>art ${item.art} qty ${item.qty}<div>`
+//}
+//str //=
+
+const articlesObj = [
+  {
+    article: "AURORA-800-2C-SO-RT",
+    qty: "7",
+  },
+  {
+    article: "CADRO-80-1C-SO-BG-BLUM",
+    qty: "3",
+  },
+  {
+    article: "KRAFT 39-500/390-2C-SO-BO",
+    qty: "5",
+  },
+  {
+    article: "KRAFT 39-500/390-2C-SO-CG",
+    qty: "5",
+  },
+  {
+    article: "KRAFT 39-500/390-2C-SO-RNN",
+    qty: "13",
+  },
+];
+
+const file = () => {
+  let result = "";
+
+  for (let i = 0; i < articlesObj.length; i += 1) {
+    const text = `
+  <File>
+    <Type>0</Type>
+    <Name>${"articles2[i].link"}</Name>
+    <SubName/>
+    <Sign>${articlesObj[i].article}</Sign>
+    <Note/>
+    <Comment/>
+    <Count>${articlesObj[i].qty}</Count>
+  </File>
+  `;
+    if (result === "") result = text;
+    else result += text;
+  }
+  return result;
+};
+
+const xml = `
+<?xml version="1.0" encoding="UTF-8"?>
+<Document>
+<DataProject>
+  <ListFiles>
+  ${file()}
+    </ListFiles>
+  <CuttingInfo>
+    <Parameters/>
+    <ProductCount>1</ProductCount>
+  </CuttingInfo>
+  <EstimateInfo/>
+</DataProject>
+</Document>
+`;
+
+console.log(xml);
