fb9e6e58827d6cc4977572e8345b306425d6df94
MaximMukhin <eriwer@gmail.com>
2022-08-08 19:59:52 +0300

    fix getOrderName

--- src/module/getOrderName.js
+++ src/module/getOrderName.js
@@ -1,6 +1,8 @@
+// получение номера заказа
 const getOrderName = () => {
   let order = "";
   order = document.getElementById("input-order").value;
+  return order;
 };
 
 export default getOrderName;
