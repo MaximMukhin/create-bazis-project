372510179d3e0ff885fa3a87e825c5dc0e5432a3
MaximMukhin <eriwer@gmail.com>
2022-08-09 10:29:40 +0300

    fix downloadProject

--- src/module/downloadProject.js
+++ src/module/downloadProject.js
@@ -4,7 +4,7 @@ const downloadProject = (order, str, error) => {
   if (error[1].length === 0) {
 
     let link = document.createElement("a");
-    link.download = `${order}.txt`;
+    link.download = `${order}.bprj`;
     let blob = new Blob([str], { type: "text/plain" });
     link.href = URL.createObjectURL(blob);
     link.click();
