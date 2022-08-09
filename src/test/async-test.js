1752b3a9ac6ff421b75fa617101439a7192abf8b
MaximMukhin <eriwer@gmail.com>
2022-08-09 20:36:11 +0300

    fix

--- src/test/async-test.js
+++ src/test/async-test.js
@@ -1,13 +1,25 @@
-const getProduct = () => {
-  fetch("https://jsonplaceholder.typicode.com/todos/1")
-    .then((response) => response.json())
-    .then((json) => console.log("getProduct", json));
-};
-getProduct();
+//const getProduct = () => {
+//  fetch("https://jsonplaceholder.typicode.com/todos/1")
+//    .then((response) => response.json())
+//    .then((json) => console.log("getProduct", json));
+//};
+//getProduct();
 
-const getProduct2 = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+const getProduct = async (url) => {
+  const response = await fetch(url);
   const data = await response.json();
-  console.log("getProduct2", data);
+  console.log("getProduct", data);
+  return data;
 };
-getProduct2();
+
+getProduct("https://jsonplaceholder.typicode.com/todos/1").then(
+  (dataPromise) => {
+    console.log("dataPromise", dataPromise);
+  }
+);
+
+getProduct("https://jsonplaceholder.typicode.com/todos/2").then(
+  (dataPromise) => {
+    console.log("dataPromise", dataPromise);
+  }
+);
