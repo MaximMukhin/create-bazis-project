26ace4f86e75cce6ca071513fb4569cf111a1874
MaximMukhin <eriwer@gmail.com>
2022-08-13 22:09:36 +0300

    добавил трай кетч

--- src/module/loadProducts.js
+++ src/module/loadProducts.js
@@ -2,17 +2,21 @@ import getProducts from "./getProducts.js";
 
 // получаю артикулы с сервера
 const loadProducts = async () => {
-  const acquaCraft = await getProducts(
-    "http://127.0.0.1:5500/products/acqua-craft.json"
-  );
-  const atrMax = await getProducts(
-    "http://127.0.0.1:5500/products/atr-max.json"
-  );
-  const iberis = await getProducts(
-    "http://127.0.0.1:5500/products/iberis.json"
-  );
-  const allLoad = [...acquaCraft, ...atrMax, ...iberis];
-  return allLoad;
+  try {
+    const acquaCraft = await getProducts(
+      "http://127.0.0.1:5500/products/acqua-craft.json"
+    );
+    const atrMax = await getProducts(
+      "http://127.0.0.1:5500/products/atr-max.json"
+    );
+    const iberis = await getProducts(
+      "http://127.0.0.1:5500/products/iberis.json"
+    );
+    const allLoad = [...acquaCraft, ...atrMax, ...iberis];
+    return allLoad;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export default loadProducts;
