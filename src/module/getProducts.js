7b1cd644badb276240b8b3e4e48d818cbf4dfde8
MaximMukhin <eriwer@gmail.com>
2022-08-13 15:06:24 +0300

    fix getProducts

--- src/module/getProducts.js
+++ src/module/getProducts.js
@@ -1,7 +1,7 @@
-const getProduct = async (url) => {
+const getProducts = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
   return data;
 };
 
-export default getProduct;
+export default getProducts;
