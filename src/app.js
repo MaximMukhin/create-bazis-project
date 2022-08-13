02fcee761494ae20ad7751b65f6137bbdb21f18c
MaximMukhin <eriwer@gmail.com>
2022-08-13 15:07:28 +0300

    сделал загрузку через loadProducts

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-import { artMax, acquaCraft, iberis } from "../products/products.js";
 import articlesPars from "./module/articlesPars.js";
 import textParsArrayList from "./module/textParsArrayList.js";
 import textParsObjectList from "./module/textParsObjectList.js";
@@ -8,49 +7,34 @@ import productToWork from "./module/productToWork.js";
 import readFile from "./module/readFile.js";
 import fileForBprj from "./module/fileForBprj.js";
 import getOrderName from "./module/getOrderName.js";
-import getProduct from "./module/getProducts.js";
+import loadProducts from "./module/loadProducts.js";
 
-//let products2 = [];//
-
-const loadProduct = async () => {
-  const acquaCraft = await getProduct(
-    "http://127.0.0.1:5500/products/acqua-craft.json"
-  );
-  const atrMax = await getProduct(
-    "http://127.0.0.1:5500/products/atr-max.json"
-  );
-  const iberis = await getProduct("http://127.0.0.1:5500/products/iberis.json");
-  const allLoad = [...acquaCraft, ...atrMax, ...iberis];
-    return allLoad
-};
-loadProduct();
-
-console.log(loadProduct())
-
-//база артикулов
-const products = [...artMax, ...acquaCraft, ...iberis];
-console.log("products", products);
+const products = loadProducts();
 
 let order = "";
 let str = "";
 let toWork = [];
 
-// запуск чтения файла
-const inputFile = document.getElementById("input-file");
-inputFile.addEventListener("change", () => {
-  const productsArr = readFile(inputFile);
-  productsArr.then((productsArr) => {
-    order = "";
-    str = "";
-    toWork = [];
-    textParsArrayList(productsArr);
-    const productObj = articlesPars(productsArr);
-    textParsObjectList(productObj);
-    const productWork = productToWork(productObj, products);
-    articlesNotFoundList(productWork);
-    str = fileForBprj(productWork);
-    order = getOrderName();
-    toWork.push(productWork);
+products.then((products) => {
+  console.log(products);
+
+  // запуск чтения файла
+  const inputFile = document.getElementById("input-file");
+  inputFile.addEventListener("change", () => {
+    const productsArr = readFile(inputFile);
+    productsArr.then((productsArr) => {
+      order = "";
+      str = "";
+      toWork = [];
+      textParsArrayList(productsArr);
+      const productObj = articlesPars(productsArr);
+      textParsObjectList(productObj);
+      const productWork = productToWork(productObj, products);
+      articlesNotFoundList(productWork);
+      str = fileForBprj(productWork);
+      order = getOrderName();
+      toWork.push(productWork);
+    });
   });
 });
 
